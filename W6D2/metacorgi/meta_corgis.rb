46dfacdecb001c0692bf816ee452eb52cd8dc790
CG <cgodlew@gmail.com>
2020-04-21 13:06:15 -0400

    Upload another attempt at metacorgis

--- W6D2/metacorgi/meta_corgis.rb
+++ W6D2/metacorgi/meta_corgis.rb
@@ -107,11 +107,15 @@ class MetaCorgiSnacks
   def initialize(snack_box, box_id)
     @snack_box = snack_box
     @box_id = box_id
+    #snack_box.methods.grep(/^get_(.*)_info$/) { MetaCorgiSnacks.define_snack $1 }
+
   end
 
   def method_missing(name, *args)
-    snacks = @snack_box.send(name)
-    snacks
+    info = @snack_box.send("get_#{name}_info", @box_id)
+    tastiness = @snack_box.send("get_#{name}_tastiness", @box_id)
+    tastiness > 30 ? "* #{result}" : result
+    # result = @snack_box.send{"Treat: #{name}: #{tastiness} "
   end
 
 
